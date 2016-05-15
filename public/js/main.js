248fcbf874085e8fabf9d6be31c52be49525e4f9
theperfectfuel <robert@theperfectfuel.com>
2016-05-15 12:59:16 -0700

    Finished creating slide toggle for recipes in recipe list view

--- public/js/main.js
+++ public/js/main.js
@@ -287,6 +287,38 @@ angular.module('beerApp', ['ngRoute', 'ngAnimate', 'auth0', 'angular-jwt'])
 			console.log(response);
 		});
 
+	}])
+
+	.animation('.slide-toggle', ['$animateCss', function($animateCss) {
+	    return {
+	        addClass: function(element, className, doneFn) {
+	            if (className == 'ng-hide') {
+	                var animator = $animateCss(element, {                    
+	                    to: {height: '0px', opacity: 0}
+	                });
+	                if (animator) {
+	                    return animator.start().finally(function() {
+	                        element[0].style.height = '';
+	                        doneFn();
+	                    });
+	                }
+	            }
+	            doneFn();
+	        },
+	        removeClass: function(element, className, doneFn) {
+	            if (className == 'ng-hide') {
+	                var height = element[0].offsetHeight;
+	                var animator = $animateCss(element, {
+	                    from: {height: '0px', opacity: 0},
+	                    to: {height: height + 'px', opacity: 1}
+	                });
+	                if (animator) {
+	                 return animator.start().finally(doneFn);
+	                }
+	            }
+	            doneFn();
+	        }
+	    };
 	}]);
 
 
